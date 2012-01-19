b13c6d71044d9e03c003f595e84498c5edcfc16b
Ryan Brainard <brainard@heroku.com>
2012-01-18 18:36:43 -0800

    fix test failure

--- test/detect_test.sh
+++ test/detect_test.sh
@@ -9,6 +9,6 @@ testDetect()
   capture ${BUILDPACK_HOME}/bin/detect ${BUILD_DIR}
   
   assertEquals 0 ${RETURN}
-  assertEquals "FAIL ON PURPOSE" "`cat ${STD_OUT}`"
+  assertEquals "Sonar" "`cat ${STD_OUT}`"
   assertNull "`cat ${STD_ERR}`"
 }
